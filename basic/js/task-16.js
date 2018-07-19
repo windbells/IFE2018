2b6e470b83a4e6601c68254a3a0d4baa1d79d7e7
nana <674700902@qq.com>
2018-07-19 13:30:06 +0800

    修改打招呼方式

--- basic/js/task-16.js
+++ basic/js/task-16.js
@@ -2,35 +2,37 @@ window.onload = function() {
 	//点击查看我的爱好按钮，展示爱好
 	var clickBtn = document.getElementById("click");
 	var hobby = document.getElementsByClassName("hobby")[0];
+	var now = new Date(),hour = now.getHours(); 
+	var time = document.getElementById("time");
 	clickBtn.onclick = function() {
 		clickBtn.style.display = "none";
 		hobby.style.display = "block";
-	}
-	
-	//判断时间
-		now = new Date(),hour = now.getHours(); 
+		
+		//判断时间
 		if(hour < 6) {
-			alert("凌晨好！");
+			time.innerHTML = "凌晨好！";
 			}
 		else if (hour < 9) {
-			alert("早上好！");
+			time.innerHTML = "早上好！";
 			}
 		else if (hour < 12) {
-			alert("上午好！");
+			time.innerHTML = "上午好！";
 			}
 		else if (hour < 14) {
-			alert("中午好！");
+			time.innerHTML = "中午好！";
 			}
 		else if (hour < 17) {
-			alert("下午好！");
+			time.innerHTML = "下午好！";
 			}
 		else if (hour < 19) {
-			alert("傍晚好！");
+			time.innerHTML = "傍晚好！";
 			}
 		else if (hour < 22) {
-			alert("晚上好！");
+			time.innerHTML = "晚上好！";
 			}
 		else {
-			alert("夜里好！");
+			time.innerHTML = "夜里好！";
 		}
+	}
+	
 }
