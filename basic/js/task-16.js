7ab0efa1bdfc7ac40a6c980816f07bacb7fca142
nana <674700902@qq.com>
2018-07-19 13:14:14 +0800

    增加当用户访问页面的时候，根据当前时间，在页面中输出不同的问候语。比如早上的时候，输出早上好，晚上的时候是晚上好。

--- basic/js/task-16.js
+++ basic/js/task-16.js
@@ -1,8 +1,36 @@
 window.onload = function() {
+	//点击查看我的爱好按钮，展示爱好
 	var clickBtn = document.getElementById("click");
 	var hobby = document.getElementsByClassName("hobby")[0];
 	clickBtn.onclick = function() {
 		clickBtn.style.display = "none";
 		hobby.style.display = "block";
 	}
+	
+	//判断时间
+		now = new Date(),hour = now.getHours(); 
+		if(hour < 6) {
+			alert("凌晨好！");
+			}
+		else if (hour < 9) {
+			alert("早上好！");
+			}
+		else if (hour < 12) {
+			alert("上午好！");
+			}
+		else if (hour < 14) {
+			alert("中午好！");
+			}
+		else if (hour < 17) {
+			alert("下午好！");
+			}
+		else if (hour < 19) {
+			alert("傍晚好！");
+			}
+		else if (hour < 22) {
+			alert("晚上好！");
+			}
+		else {
+			alert("夜里好！");
+		}
 }
