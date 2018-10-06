46337a169c7a58e7bb460ea4b56921281fde9d99
nana <674700902@qq.com>
2018-10-06 23:51:16 +0800

    MVVM学院

---
+++ mvvm/task1-2/src/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        {{text}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data () {
+            return {
+                text: 'this is test'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #app {
+        color: red;
+    }
+</style>
